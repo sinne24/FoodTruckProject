e425b538a8efaabb22d1ffa0775ad5e2ac25e610
Cindy <cindy@Students-MacBook-Pro.local>
2021-07-31 10:50:40 -0600

    Completed code template and instantiated FoodTrackApp object to begin calling function methods from main

--- src/com/skilldistillery/foodtruck/app/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruck.java
@@ -1,5 +1,55 @@
 package com.skilldistillery.foodtruck.app;
 
 public class FoodTruck {
+	private static int uniqueID = 1112;
+	private String truckName;
+	private String foodType;
+	private int starRating; //0 - 5 stars
+	
+	public FoodTruck(String truckName, String foodType, int starRating) {
+		super();
+		FoodTruck.uniqueID++;
+		int truckId = this.uniqueID;
+		this.truckName = truckName;
+		this.foodType = foodType;
+		this.starRating = starRating;	
+		
+	}
+
+	public static int getUniqueID() {
+		return uniqueID;
+	}
+
+	public static void setUniqueID(int uniqueID) {
+		FoodTruck.uniqueID = uniqueID;
+	}
+
+	public String getTruckName() {
+		return truckName;
+	}
+
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getStarRating() {
+		return starRating;
+	}
+
+	public void setStarRating(int starRating) {
+		this.starRating = starRating;
+	}
+	
+	
+	
+	
 
 }
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,90 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
+		boolean quit = false;
+		int selection;
+		Scanner kb = new Scanner(System.in);
+
+		FoodTruck[] lineUp = new FoodTruck[5];
+		FoodTruckApp truckApp = new FoodTruckApp();
+
+		System.out.println(
+				"Enter the food truck line up! Prompts will appear asking " 
+		+ "for specific information on each truck");
+		lineUp = truckApp.populateTrucks(kb);
+
+		while (!quit) {
+			truckApp.printMenu();
+			selection = kb.hashCode();
+			truckApp.userSelection(selection);
+			if (selection == 4) {
+				quit = true;
+			}
+
+		}
+
+	}
+
+	private void userSelection(int selection) {
+		if (selection == 1) {
+			System.out.println("Listing all existing food trucks: ");
+		} else if (selection == 2) {
+			int average = calculateAvgRating();
+			System.out.println("The average rating of these food trucks is " + average);
+
+		} else if (selection == 3) {
+			String highestRatedTruck = determineHighestRatedTruck();
+			System.out.println("The highest-rated food truck is" + highestRatedTruck);
+		} else {
+			System.out.println("Quitting. Thanks for stopping by!");
+		}
+
+	}
+
+	private String determineHighestRatedTruck() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private int calculateAvgRating() {
 		// TODO Auto-generated method stub
 
+	}
+
+	private void printMenu() {
+		System.out.println("1.)List all existing food trucks.\n"
+						 + "2.)See the average rating of food trucks.\n"
+						 + "3.)Display the highest-rated food truck.\n"
+						 + "4.)Quit the program.");
+
+	}
+
+	private FoodTruck[] populateTrucks(Scanner kb) {
+		String foodTruckName;
+		int starRating;
+		String foodType;
+		FoodTruck newTruck;
+		FoodTruck[] lineUp = new FoodTruck[5];
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Please enter the food truck's name or quit to stop: ");
+			foodTruckName = kb.nextLine();
+			if (foodTruckName.equalsIgnoreCase("quit")) {
+				break;
+			}
+			System.out.println("Please enter the type of food " + foodTruckName + " serves: ");
+			foodType = kb.nextLine();
+			System.out.println("Please enter how many stars you rate " + foodTruckName + " at (0-5): ");
+			starRating = kb.nextInt();
+
+			newTruck = new FoodTruck(foodTruckName, foodType, starRating);
+			lineUp[i] = newTruck;
+		}
+		return lineUp;
 	}
 
 }
